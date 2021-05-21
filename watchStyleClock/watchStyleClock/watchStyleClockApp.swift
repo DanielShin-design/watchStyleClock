077b13e5316c70e248e3a402dec8557260da15f8
Daniel Shin <clickdaniel@mac.com>
2021-05-21 17:48:52 +0900

    시작하기

---
+++ watchStyleClock/watchStyleClock/watchStyleClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchStyleClockApp.swift
+//  watchStyleClock
+//
+//  Created by iM27 on 2021/05/21.
+//
+
+import SwiftUI
+
+@main
+struct watchStyleClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
